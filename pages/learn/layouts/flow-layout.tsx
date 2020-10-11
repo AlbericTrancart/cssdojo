f6f9957776e01fe8c68e22f3db8cfe02709bd118
Albéric Trancart <alberic@trancart.net>
2020-10-11 15:51:59 +0200

    Make empty pages for empty katas

---
+++ pages/learn/layouts/flow-layout.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next';
+import { Subtitle, Title } from 'components/Layout';
+import { Exercise } from 'components/Exercise';
+import { Image } from 'components/Image';
+import { Code } from 'components/Code';
+import { Link } from 'components/Link';
+import { NextKataButton } from 'components/NextKataButton';
+import { Editor } from 'components/Editor';
+import { PAGES } from 'services/pages';
+
+const Kata: NextPage = () => (
+  <>
+    <Title>The flow layout</Title>
+
+    <p>This kata is not written yet ;)</p>
+  </>
+);
+
+export default Kata;
