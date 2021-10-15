22618e267e3f85a45ba34bf30d54376ec60b318d
Albéric Trancart <alberic@trancart.net>
2021-10-15 15:05:03 +0200

    Add opengraph tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -38,6 +38,17 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <meta property="og:url" content={`https://cssdojo.dev${router.pathname}`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content="(re)learn CSS, the right way" />
+        <meta property="og:site_name" content="CSS Dojo" />
+        <meta property="og:image" content="https://cssdojo.dev/android-chrome-512x512.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+        <meta property="og:image:alt" content="The CSS3 logo" />
       </Head>
 
       <CSSResets />
